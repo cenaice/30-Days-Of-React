f7ac600856173e097aaa414e2726c3ecce3b77c4
Victor <victorphiathep@gmail.com>
2023-08-12 13:44:00 -0400

    FInished userCard with MaterialUI styling

--- 03_Day_Setting_Up/day-3-create-react-app/src/index.js
+++ 03_Day_Setting_Up/day-3-create-react-app/src/index.js
@@ -1,8 +1,59 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import Chip from "@mui/material/Chip";
+import avatar from "./images/asian-man.jpg";
+import { spacing } from '@mui/system';
+import Box from "@mui/material/Box";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+// User Card
+
+// USER CARD STYLING
+
+const avatarStyle = {
+  verticalAlign: "middle",
+  width: "300px",
+  height: "300px",
+  borderRadius: "50%",
+  padding: "10px",
+};
+
+// USER CALL HTML CODE
+
+const userCard = (
+  <Box className="userCard-wrapper">
+    <Card variant="outlined">
+      <Box className="user_info" m={5}>
+        <img src={avatar} alt="Avatar" style={avatarStyle}></img>
+        <Typography variant="h1" spacing="">Victer Phiathep</Typography>
+        <svc data-testid="CheckCircleIcon"></svc>
+        <Typography variant="body1">
+          Junior Software Engineer, New York City
+        </Typography>
+      </Box>
+      <Box className="user_skills" m={5}>
+        <Typography variant="h3">Skills</Typography>
+        <Box m={2}>
+          <Chip label="ReactJS" color="primary" />
+          <Chip label="TypeScript" color="primary" />
+          <Chip label="MongoDB" color="primary" />
+          <Chip label="NextJS" color="primary" />
+          <Chip label="Python" color="primary" />
+          <Chip label="HTML" color="primary" />
+          <Chip label="CSS" color="primary" />
+          <Chip label="JavaScript" color="primary" />
+        </Box>
+        <Typography varaint="subtitle2">Joined on Aug 12, 2023</Typography>
+      </Box>
+    </Card>
+  </Box>
+);
+
+// ----------------------------------------------------------------------------------------------------
 
 const mainWrapperStyles = {
   margin: "auto",
@@ -36,13 +87,23 @@ const buttonSpace = {
 const main = (
   <div className="main-wrapper" style={mainWrapperStyles}>
     <div className="text-wrapper" style={textWrapperStyle}>
-      <h1>Subscribe</h1>
-      <p>Sign up with your email address to receive news and updates</p>
+      <Typography variant="h3">Subscribe</Typography>
+      <Typography>Sign up with your email address to receive news and updates</Typography>
     </div>
     <div className="input_box-wrapper" style={inputBoxStyle}>
-    <TextField id="first_name" label="First Name" variant="filled" />
-    <TextField style={buttonSpace} id="last_name" label="Last Name" variant="filled" />
-    <TextField style={buttonSpace} id="email" label="Email" variant="filled" />
+      <TextField id="first_name" label="First Name" variant="filled" />
+      <TextField
+        style={buttonSpace}
+        id="last_name"
+        label="Last Name"
+        variant="filled"
+      />
+      <TextField
+        style={buttonSpace}
+        id="email"
+        label="Email"
+        variant="filled"
+      />
     </div>
     <div className="subscribe-wrapper" style={subscribeStyle}>
       <Button variant="contained">Subscribe</Button>
@@ -50,14 +111,10 @@ const main = (
   </div>
 );
 
-// User Card
-
-const userCard = <div></div>;
-
 // JSX element, app
 const app = (
   <div className="app">
-    {main},{userCard}
+    {userCard},{main}
   </div>
 );
 
